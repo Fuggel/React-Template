018f3e1b0d9150d9a5ef22d5bafbb9039a60f8c1
Fuggel <furkanserdarceylan@gmail.com>
2023-10-03 14:57:25 +0200

    test: adjust e2e test for toggleSwitch

--- cypress/e2e/components/layouts/sidebar.cy.ts
+++ cypress/e2e/components/layouts/sidebar.cy.ts
@@ -8,8 +8,7 @@ describe("Sidebar", () => {
     it("should display the sidebar and toggle dark mode", () => {
         cy.get("[data-testid='sidebar']").should("be.visible");
 
-        cy.get("[data-testid='icon']")
-            .last()
+        cy.get(".dark-mode")
             .click()
             .get(".app")
             .should("have.css", "background-color", "rgb(22, 22, 22)"); // Convert HEX to RGB
